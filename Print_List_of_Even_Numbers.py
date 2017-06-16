191f7a29b1d0e7bc527f4b2da303dc2460d6dd5a
joson-domrix <domrix.dj.joson@gmail.com>
2017-06-16 17:07:29 +0800

    Update Print_List_of_Even_Numbers.py

--- Print_List_of_Even_Numbers.py
+++ Print_List_of_Even_Numbers.py
@@ -1,2 +1,7 @@
-print [x for x in range(2, 100) if not x % 2]
-print range(2, 100, 2)
+
+for x in range (2,100): # for loop , x is a variable then ranging from whatever inside the parameter is the argument.
+    if (x%2)==0: #condition.. if x divided by 2 gets remainder of 0, its an even number
+    
+        print (x)#if condition is true, print value of x
+    else:
+        pass # do nothing
